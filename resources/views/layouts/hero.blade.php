fe93777b5b1220a219f59ab68449295d5b7c413f
RodrigoFreireA <rodrigofandradee@gmail.com>
2023-08-18 15:39:58 -0300

    Adicionado a hero section

---
+++ resources/views/layouts/hero.blade.php
@@ -0,0 +1,15 @@
+
+        
+<div class="hero-content">
+        <div class="p-5 text-center bg-image" style="
+            background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');
+            height: 400px;
+        ">
+            <div class="text-white">
+                <h1 class="mb-3">Heading</h1>
+                <h4 class="mb-3">Subheading</h4>
+                <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+            </div>
+        </div>
+    </div>
+</div>
